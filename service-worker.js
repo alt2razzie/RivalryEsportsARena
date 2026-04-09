3c061672699221e106623bace66664d8fc0e8b5b
alt2razzie <alt2razzie@gmail.com>
2026-04-10 03:42:46 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'rvl-hub-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
